7a9da7915a392f63f9d50cf82cb3bd55d8dbc903
alamin2016mdy <alamin2016mdy@gmail.com>
2023-07-28 15:44:44 +0600

    iubi

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.write("<h1>my heading</h1> <br/>");
+document.write("Name: Al Amin <br/>");
+document.write("Number: <i>0123456789</i><br/>");
+document.write("<table><tr><th>name</th><th>number</th><th>mail</th></tr><tr><td>41</td><td>65</td><td>74</td></tr><tr><td>16</td><td>14</td><td>10</td></tr></table>")
